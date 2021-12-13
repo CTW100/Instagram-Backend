52014393ae40c7465557d4e93a657819e8db23ad
CTW100 <85342844+CTW100@users.noreply.github.com>
2021-12-13 18:56:04 +0900

    #3.6 Prisma Client

--- server.js
+++ server.js
@@ -1,34 +1,45 @@
+// prisma client를 import해서 새로운 client를 생성할 것임. client는 prisma를 dev로 migrate할 때 prisma가 자동생성해서 node_modules에 넣어놓음
+import { Prisma, PrismaClient } from '@prisma/client';
 import { ApolloServer, gql } from 'apollo-server';
+
+const client = new PrismaClient();
 
 const typeDefs = gql`
   type Movie {
-    title: String
-    year: Int
+    id: Int!
+    title: String!
+    year: Int!
+    genre: String
+    createdAt: String!
+    updatedAt: String!
   }
 
   type Query {
     movies: [Movie]
-    movie: Movie
+    movie(id: Int!): Movie
   }
 
   type Mutation {
-    createMovie(title: String!): Boolean
-    deleteMovie(title: String!): Boolean
+    createMovie(title: String!, year: Int!, genre: String): Movie
+    deleteMovie(id: String!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    movies: () => [],
-    movie: () => ({ title: 'Hello', year: 2021 }),
+    movies: () => client.movie.findMany(),
+    movie: (_, { id }) => ({ title: 'Hello', year: 2021 }),
   },
   Mutation: {
-    createMovie: (_, { title }) => {
-      console.log(title);
-      return true;
-    },
+    createMovie: (_, { title, year, genre }) =>
+      client.movie.create({
+        data: {
+          title,
+          yearm,
+          genre,
+        },
+      }),
     deleteMovie: (_, { title }) => {
-      console.log(title);
       return true;
     },
   },
