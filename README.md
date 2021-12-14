445f88fdad6cd934bfdfcaaeff0cc2ebddb6dc50
CTW100 <85342844+CTW100@users.noreply.github.com>
2021-12-14 15:33:50 +0900

    #3.10 Architecture part Two

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ npx prisma studio --> 스튜디오는 기본적으로 우리의 DB에 있는 MOD
 # 3.9
 
 resolver 랑 type을 적는 방식이 구림. 한 곳에 다 넣음. divide and conquer 하자.
+
+# 3.10
+
+npm i graphql-tools --> 모든 mutations query typeDefs 끼리끼리 임포트하고 싶을 때 쓰기 위함
---
+++ schema.js
@@ -0,0 +1,20 @@
+import {
+  loadFilesSync,
+  makeExecutableSchema,
+  mergeResolvers,
+  mergeTypeDefs,
+} from 'graphql-tools';
+
+const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
+// pattern language임. ** 모든 폴더 안 * 모든 파일 이든 typeDef.js로 끝나는 애들
+// loadFilesSync 는 export default 밖에 못불러옴. 그래서 movies.mutations 랑 movies.queries, movies.typeDefs 는 export default 로 해줘야 함.
+const loadedResolvers = loadFilesSync(
+  `${__dirname}/**/*.{queries, mutations}.js` // {queries, mutations} -> 둘 다 ok
+);
+
+const typeDefs = mergeTypeDefs(loadedTypes);
+const resolvers = mergeResolvers(loadedResolvers);
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+export default schema;
