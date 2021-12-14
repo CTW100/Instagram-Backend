9dea6ce6e42728e4a9397bec6c6c732a51bdcea6
CTW100 <85342844+CTW100@users.noreply.github.com>
2021-12-14 15:47:50 +0900

    #3.11 Dotenv

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ resolver 랑 type을 적는 방식이 구림. 한 곳에 다 넣음. divide and 
 # 3.10
 
 npm i graphql-tools --> 모든 mutations query typeDefs 끼리끼리 임포트하고 싶을 때 쓰기 위함
+
+# 3.11
+
+가상환경에서 우리의 환경 변수를 읽을 수 있도록 설정할 것
+
+npm i dotenv --> app의 가장 먼저에서 불러야 함
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+require('dotenv').config(); // dotenv를 불러들이면서 곧바로 dotenv의 config 메서드를 실행시킴  =  import dotenv from 'dotenv' and dotenv.config(). process.env 로 환경변수에 접근 가능
+
 import { ApolloServer } from 'apollo-server';
 import schema from './schema';
+
+const POST = process.env.PORT;
 
 const server = new ApolloServer({
   schema,
 });
+// 원래
+// const server = new ApolloServer({
+//   typeDefs: typeDefs,
+//   resolvers: resolvers
+// })
+// 임.
 
 server
-  .listen()
-  .then(() => console.log('Server is running on http://localhost:4000/'));
+  .listen(PORT)
+  .then(() => console.log('Server is running on http://localhost:${PORT}/'));
