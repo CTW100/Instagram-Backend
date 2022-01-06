636831afd7c4aaa34df7251662ac07c6a81c014e
CTW100 <85342844+CTW100@users.noreply.github.com>
2022-01-06 16:22:04 +0900

    #6.7 Revised (version right before was changed editPhoto resolver with searchPhotos resolver

--- photos/editPhoto/editPhoto.resolvers.js
+++ photos/editPhoto/editPhoto.resolvers.js
@@ -1,14 +1,32 @@
 import client from '../../client';
+import { protectedResolver } from '../../users/users.utils';
 
 export default {
-  Query: {
-    searchPhotos: (_, { keyword }) =>
-      client.photo.findMany({
-        where: {
-          caption: {
-            startsWith: keyword,
+  Mutation: {
+    editPhoto: protectedResolver(
+      async (_, { id, caption }, { loggedInUser }) => {
+        const ok = await client.photo.findFirst({
+          where: {
+            id,
+            userId: loggedInUser.id, // userId 는 loggedInUser의 id와 일치하는 사진 찾는 것임
           },
-        },
-      }),
+        });
+        if (!ok) {
+          return {
+            ok: false,
+            error: 'Photo not found.',
+          };
+        }
+        const photo = await client.photo.update({
+          where: {
+            id,
+          },
+          data: {
+            caption: caption,
+          },
+        });
+        console.log(photo);
+      }
+    ),
   },
 };
--- photos/searchPhotos/searchPhotos.resolvers.js
+++ photos/searchPhotos/searchPhotos.resolvers.js
@@ -1,32 +1,14 @@
 import client from '../../client';
-import { protectedResolver } from '../../users/users.utils';
 
 export default {
-  Mutation: {
-    editPhoto: protectedResolver(
-      async (_, { id, caption }, { loggedInUser }) => {
-        const ok = await client.photo.findFirst({
-          where: {
-            id,
-            userId: loggedInUser.id, // userId 는 loggedInUser의 id와 일치하는 사진 찾는 것임
+  Query: {
+    searchPhotos: (_, { keyword }) =>
+      client.photo.findMany({
+        where: {
+          caption: {
+            startsWith: keyword,
           },
-        });
-        if (!ok) {
-          return {
-            ok: false,
-            error: 'Photo not found.',
-          };
-        }
-        const photo = await client.photo.update({
-          where: {
-            id,
-          },
-          data: {
-            caption: caption,
-          },
-        });
-        console.log(photo);
-      }
-    ),
+        },
+      }),
   },
 };
